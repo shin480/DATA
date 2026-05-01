659f255665fd9267f507f483c76950b6c87494a2
윤예나 <yena@naver.com>
2026-05-01 11:35:42 +0900

    ai 돌아가유

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 import pandas as pd
 from util.db import get_engine
+from model.model_main import app as pipeline_app
+
 app = FastAPI()
 app.mount("/view", StaticFiles(directory="view"))
+# 파이프라인 앱을 통째로 "/pipeline" 주소에 마운트
+app.mount("/pipeline", pipeline_app)
 
 @app.get("/")
 def main():
-    return RedirectResponse("view/index.html")
+    return RedirectResponse("/view/main.html")
 
 # 테이블 내용 가져오기 테스트
 @app.get('/read_table')
@@ -16,3 +20,4 @@ def read_table():
     df = pd.read_sql_table(table_name='users', con=get_engine())
     print(df.head())
     return df.to_dict() # http://127.0.0.1:8000/read_table
+
