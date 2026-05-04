a0ae36e36faa34158aa269e62be731c3609dcff8
윤예나 <yena@naver.com>
2026-05-04 12:29:28 +0900

    약관동의 세션저장 완료

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from typing import Dict, List
+
 from fastapi import FastAPI
+from starlette.requests import Request
 from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+
 import pandas as pd
 from util.db import get_engine
 from model.model_main import app as pipeline_app
 from datacleaning.cleaning import get_preprocessed_data
+from pydantic import BaseModel
 
 app = FastAPI()
 app.mount("/view", StaticFiles(directory="view"))
 # 파이프라인 앱을 통째로 "/pipeline" 주소에 마운트
 app.mount("/pipeline", pipeline_app)
+
+app.add_middleware(SessionMiddleware, secret_key="motmachugetjyo")
 
 @app.get("/")
 def main():
@@ -27,3 +35,20 @@ def read_table():
 def data_cleaning():
     result = get_preprocessed_data()  # 아까 만든 전처리 함수 호출
     return result  # 처리 건수나 성공 메시지를 화면에 출력
+
+# 이용약관 구조 정의
+class TermsRequest(BaseModel):
+    requiredChecks: bool  # 또는 str, 실제 데이터에 맞게 설정
+    optionalChecksList: List[str] # 리스트 형태임을 명시
+
+@app.post('/terms')
+def terms(info: TermsRequest, req: Request):
+    print(f"필수 동의 여부 : {info.requiredChecks} / 선택 동의 리스트 {info.optionalChecksList}")
+    req.session["requiredChecks"] = info.requiredChecks
+    req.session["optionalChecksList"] = info.optionalChecksList
+    return info
+
+@app.get("/regist")  # 브라우저가 /regist로 접속하면
+def show_regist_page(req: Request):
+    # templates 폴더에 있는 regist.html을 찾아서 브라우저에게 던져줌!
+    return {"request": req}
